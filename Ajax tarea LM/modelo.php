177694b8adfa6b6cdfe57794d2698ffe0638d7a4
OmarChaaban10 <omarmounder.s454046@cesurformacion.com>
2025-05-23 14:38:29 +0200

    Avance de tarea

---
+++ Ajax tarea LM/modelo.php
@@ -0,0 +1,27 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+?>
+
+ <li>
+    <span>Nombre:</span>
+    <span>Omar</span>
+</li>
+<li>
+    <span>Apellidos:</span>
+    <span>Chaaban Yaber</span>
+</li>
+<li>
+    <span>Ciclo:</span>
+    <span>DAW</span>
+</li>
+<li>
+    <span>Curso:</span>
+    <span>1ero</span>
+</li>
+<li>
+    <span>Asignatura:</span>
+    <span>Lenguaje de Marcas</span>
+</li>
+<li>
+    <span>Profesor: David Postigo</span>
+</li>
