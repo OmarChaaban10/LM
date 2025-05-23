925799bfd98b707daa9a741842f28fb37e38bded
omarchaaban10 <omarmounder.s454046@cesurformacion.com>
2025-05-23 17:30:09 +0200

    Tarea de Ajax terminada

--- Ajax tarea LM/modelo.php
+++ Ajax tarea LM/modelo.php
@@ -3,24 +3,19 @@ header("Access-Control-Allow-Origin: *");
 ?>
 
  <li>
-    <span>Nombre:</span>
-    <span>Omar</span>
+    <span>Nombre: Omar Mounder</span>
 </li>
 <li>
-    <span>Apellidos:</span>
-    <span>Chaaban Yaber</span>
+    <span>Apellidos: Chaaban Yaber</span>
 </li>
 <li>
-    <span>Ciclo:</span>
-    <span>DAW</span>
+    <span>Ciclo: DAW</span>
 </li>
 <li>
-    <span>Curso:</span>
-    <span>1ero</span>
+    <span>Curso: 1ero</span>
 </li>
 <li>
-    <span>Asignatura:</span>
-    <span>Lenguaje de Marcas</span>
+    <span>Asignatura: Lenguaje de Marcas</span>
 </li>
 <li>
     <span>Profesor: David Postigo</span>
